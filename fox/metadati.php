eb14907452c24ecfa0b55e1ab81844ff96c04ff0
radiostudiodoc <giocic75@gmail.com>
2025-05-25 22:11:45 +0200

    Update metadati.php

--- fox/metadati.php
+++ fox/metadati.php
@@ -1,13 +1,1 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: text/plain");
 
-$metadati = @file_get_contents("https://radio.radiostudiodoc.org/NowPlaying.txt");
-
-if ($metadati === false) {
-    http_response_code(500);
-    echo "Errore nel recupero dei metadati";
-} else {
-    echo $metadati;
-}
-?>
