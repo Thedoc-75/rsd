f9183666c719dc622e07dea69e225d10ee19d467
radiostudiodoc <giocic75@gmail.com>
2025-05-27 10:23:52 +0200

    main

--- fox/js/main.js
+++ fox/js/main.js
@@ -1,19 +1,25 @@
-;(function($) {
-  'use strict';
+;(function () {
+    'use strict';
 
-  var wowAnimation = function() {
-    var wow = new WOW({
-      animateClass: 'animated',
-      offset: 150,
-      callback: function(box) {
-        console.log("WOW: animating <" + box.tagName.toLowerCase() + ">");
-      }
-    });
-    wow.init();
-  };
+    var wowAnimation = function() {
+        var wow = new WOW({
+            animateClass: 'animated',
+            offset: 150,
+            callback: function(box) {
+                console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+            }
+        });
+        wow.init();
+    };
 
-  $(document).ready(function() {
-    wowAnimation();
-  });
+    // Aspetta che jQuery sia definito e che il DOM sia pronto
+    if (typeof jQuery !== "undefined") {
+        jQuery(function($) {
+            wowAnimation();
+        });
+    } else {
+        console.error("jQuery non è stato caricato correttamente.");
+    }
 
-})(jQuery);
+})();
+
