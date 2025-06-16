19df241e450143590398d414895b56f5d8294740
radiostudiodoc <giocic75@gmail.com>
2025-06-16 15:46:09 +0200

    Create service-worker.js

---
+++ fox/service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'rsd-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './css/style.css',
+  './images/radiostudiodoc.png',
+  './images/RSDFAVICON.png',
+  './images/icons/icon-192x192.png',
+  './images/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
